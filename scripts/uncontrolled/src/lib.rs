30c97b9ac89e139dcdc8d4dd1d9516e3d3aceae7
owenns <owens@gatech.edu>
2021-04-19 18:31:10 -0400

    update lib.rs

--- scripts/uncontrolled/src/lib.rs
+++ scripts/uncontrolled/src/lib.rs
@@ -1,7 +1,39 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[macro_use]
+extern crate gdnative;
+
+use gdnative::api::MeshInstance;
+use gdnative::prelude::NativeClass;
+use gdnative::prelude::Sprite;
+use gdnative::prelude::*;
+
+#[derive(gdnative::NativeClass)]
+#[inherit(gdnative::Sprite)]
+struct Uncontrolled {}
+
+#[gdnative::methods]
+impl Uncontrolled {
+    fn _init(_owner: gdnative::Sprite) -> Self {
+        Uncontrolled {}
+    }
+
+    #[export]
+    unsafe fn _ready(&mut self, mut owner: gdnative::Sprite) {
+        godot_warn!("Start");
+    }
+
+    #[export]
+    unsafe fn _process(&mut self, mut owner: gdnative::Sprite) {
+        godot_dbg!("process called");
+        let mut pos = owner.get_position();
+        pos.x += 1.0;
+        owner.set_position(pos);
     }
 }
+
+fn init(handle: InitHandle) {
+    handle.add_class::<Uncontrolled>();
+}
+
+godot_gdnative_init!();
+godot_nativescript_init!(init);
+godot_gdnative_terminate!();
